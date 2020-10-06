e80ca7c947aee73783cfccf79cb614a3acff5b1c
Ryo-15 <tomorrows.way15@gmail.com>
2020-10-06 15:05:30 +0900

    [finish]lesson19

--- work/web/result.php
+++ work/web/result.php
@@ -2,13 +2,14 @@
 
 require('../app/functions.php');
 
-$color = filter_input(INPUT_GET, 'color') ?? 'None selected';
+$colorFromGet = filter_input(INPUT_GET, 'color') ?? 'None selected';
+setcookie('color', $colorFromGet);
 
 include('../app/_parts/_header.php');
 
 ?>
 
-<p><?= h($color); ?></p>
+<p><?= h($colorFromGet); ?></p>
 <p><a href="index.php">Go back</a></p>
 <?php
 
