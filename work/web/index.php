6bd47b061c412624d6c8870c98b998a3bf1877fe
Ryo-15 <tomorrows.way15@gmail.com>
2020-10-06 12:30:43 +0900

    [finish]lesson12

--- work/web/index.php
+++ work/web/index.php
@@ -7,7 +7,7 @@ include('../app/_parts/_header.php');
 ?>
 
 <form action="result.php" method="get">
-  <input type="text" name="message">
+  <textarea name="message" id="" cols="30" rows="10"></textarea>
   <button>Send</button>
 </form>
 
