9f0df2fa18dd468312ddd418a93e7aa440537afa
Ryo-15 <tomorrows.way15@gmail.com>
2020-10-07 21:05:26 +0900

    [finish]lesson27

--- work/web/index.php
+++ work/web/index.php
@@ -2,13 +2,21 @@
 
 require('../app/functions.php');
 
-
+$filename = '../app/messages.txt';
+$messages = file($filename, FILE_IGNORE_NEW_LINES);
 
 include('../app/_parts/_header.php');
 
 ?>
 
+<ul>
+  <?php foreach ($messages as $message): ?>
+    <li><?= h($message); ?></li>
+  <?php endforeach; ?>
+</ul>
+
 <form action="result.php" method="post">
+  <input type="text" name="message">
   <button>Post</button>
 </form>
 
